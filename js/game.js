94b418d057b5fa4d885d302474cf40d26ca3c887
Alexandra Maero <alexandramaero12@gmail.com>
2020-11-06 19:26:21 -0300

    2.animating the canvas

--- js/game.js
+++ js/game.js
@@ -1,14 +1,40 @@
 var canvas = null,
     ctx = null;
+var x = 50,
+    y = 50;
+
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 17);
+        };
+}());
 
 function paint(ctx) {
-    ctx.strokeStyle = '#ff637d';
-    ctx.strokeRect(50, 50, 100, 60);
+    ctx.fillStyle = '#F4F1BB';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = '#ff637d';
+    ctx.fillRect(x, y, 10, 10);
 }
-
+function act(){
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+}
+function run(){
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+}
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    run ();
+    act();
     paint(ctx);
 }
 
